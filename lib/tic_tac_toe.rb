347f35fe1635f17926dd706763d6dc2f4f728bbd
Cristal Alatorre Perez <cristalcodes@gmail.com>
2019-10-20 16:08:22 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -24,14 +24,17 @@ class TicTacToe
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 
+
   def input_to_index(input)
     input_integer=input.to_i
     input_integer-=1
   end
 
+
   def move(index, string="X")
     @board[index]=string
   end
+
 
   def position_taken?(index)
      if @board[index]=="X" || @board[index]=="O"
@@ -41,20 +44,24 @@ class TicTacToe
      end
   end
 
+
   def valid_move?(input)
     if !self.position_taken?(input) && input.between?(0,8)
       return true
     end
   end
 
+
   def turn_count
     #@board.count("X") + @board.count("O")
     @board.count {|token| token=="X"||token=="O"}
   end
 
+
   def current_player
     self.turn_count%2==0 ? "X" : "O"
   end
+
 
   def turn
     input=gets
@@ -68,9 +75,74 @@ class TicTacToe
   end
 
   def won?
-    if @board.each_index.select {|index| @board[index] == "X"} == WIN_COMBINATIONS.any?
+    WIN_COMBINATIONS.each do |win_combo|
+     index = win_combo[0]
+     index1 = win_combo[1]
+     index2 = win_combo[2]
+
+     position1 = @board[index]
+     position2 = @board[index1]
+     position3 = @board[index2]
+
+     if position1 == position2 && position2 == position3
+       return win_combo
+     end
+    end
+   return false
+  end
+
+  def full?
+    @board.all?{|string| string == "X" || string == "O"}
+  end
+
+
+  def draw?
+    !won? && full?
+  end
+
+
+  def over?
+    if draw? || won?
       return true
+    else
+      return false
     end
   end
+
+
+  def winner
+    if won?
+      winners_combo = won?
+    end
+
+    if @board[winners_combo[0]] == "X"
+        return "X"
+    elsif @board[winners_combo[0]] == "O"
+        return "O"
+    else !won? && full?
+      return nil
+   end
+  end
+
+
+
+  def play
+    until over?
+      turn
+    end
+
+    if !over?
+      turn
+    elsif over? && won?
+      if winner=="X"
+        puts "Congratulations X!"
+      else winner=="O"
+        puts "Congratulations O!"
+      end
+    else over? && draw?
+        puts "Cat's Game!"
+    end
+  end#closes def play
+
 
 end
